f88a9fb13d358689f2f4528b1f9767225335ff9d
Lucas Capistrant <lucas.o.capistrant@gmail.com>
2024-07-31 11:08:34 -0500

    customButton component styles

--- src/components/CustomBtn.vue
+++ src/components/CustomBtn.vue
@@ -11,3 +11,21 @@
     }
   }
 </script>
+
+<style scoped>
+button {
+  background: none;
+  outline: none;
+  border: 2px solid var(--color-dark);
+  padding: .5rem 1rem;
+  border-radius: 20px;
+  font-size: 2rem;
+  cursor: pointer;
+  transition: all .2s;
+}
+
+button:hover {
+  background: var(--color-dark);
+  color: var(--color-light);
+}
+</style>
